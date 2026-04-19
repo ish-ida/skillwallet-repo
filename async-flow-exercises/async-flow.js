7103bc323461931721c4e39431e5582148f4036a
ish-ida <akirahibaru@gmail.com>
2026-04-19 17:36:50 +0800

    add folder w/files

---
+++ async-flow-exercises/async-flow.js
@@ -0,0 +1,46 @@
+// step 1
+console.log("A");
+console.log("B");
+console.log("C");
+
+// step 2
+console.log("Start");
+
+setTimeout(() => {
+  console.log("Timeout");
+}, 0);
+
+console.log("End");
+
+// step 3
+console.log("Start");
+
+Promise.resolve().then(() => {
+  console.log("Promise");
+});
+
+console.log("End");
+
+// step 4
+async function test() {
+  console.log("1");
+  await Promise.resolve();
+  console.log("2");
+}
+
+console.log("3");
+test();
+console.log("4");
+
+console.log("A");
+
+// step 5
+setTimeout(() => {
+  console.log("B");
+}, 0);
+
+Promise.resolve().then(() => {
+  console.log("C");
+});
+
+console.log("D");
